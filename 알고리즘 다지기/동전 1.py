6db5723263f4b24b4c7b4b16bb91d5909faa7579
JeongYoungHyeon <jcs5150@naver.com>
2023-02-23 15:11:22 +0900

    Clear 동전 1

--- 알고리즘 다지기/동전 1.py
+++ 알고리즘 다지기/동전 1.py
@@ -1,31 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-def noc_coin(value_left, i):
-    global noc
-    global values
-    global n
-
-    if (i >= n):
-        return ;
-    
-    if (i == n-1):
-        if (value_left % values[i] == 0):
-            noc += 1    
-        return ;
-        
-    for cnt_coin in range(int(value_left / values[i]) + 1):
-        if (value_left - (values[i] * cnt_coin) == 0):
-            noc += 1
-            return ;
-        noc_coin(value_left - (values[i] * cnt_coin), i + 1)
-
 n, k = map(int, input().split())
 
 values = []
 for i in range(n):
     values.append(int(input()))
 
-noc = 0
-noc_coin(k, 0)
-print(noc)
+dp = [0 for i in range(k+1)]
+dp[0] = 1
+
+for i in values:
+    for j in range(i, k+1):
+        if (j-i) >= 0:
+            dp[j] += dp[j-i]
+
+print(dp[j])
